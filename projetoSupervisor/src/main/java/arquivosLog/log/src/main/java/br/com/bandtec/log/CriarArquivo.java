f4b2c0d79419eb7e17120de8eedcf846cd2409d2
lui-anttunes <61800310+lui-anttunes@users.noreply.github.com>
2020-11-19 21:47:21 -0300

    Inicio-ArqLog

---
+++ projetoSupervisor/src/main/java/arquivosLog/log/src/main/java/br/com/bandtec/log/CriarArquivo.java
@@ -0,0 +1,21 @@
+package br.com.bandtec.log;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+public class CriarArquivo {
+        public static void main(String[] args) {
+            try {
+                Log supervisorErros = new Log("log.txt");
+                
+                supervisorErros.logger.setLevel(Level.WARNING);
+                
+                supervisorErros.logger.info("info msg");
+                supervisorErros.logger.warning("aviso msg");
+                supervisorErros.logger.severe("severe msg");
+                
+            } catch (IOException | SecurityException e) {
+            }
+    }
+    
+}
---
+++ projetoSupervisor/src/main/java/arquivosLog/log/src/main/java/br/com/bandtec/log/Log.java
@@ -0,0 +1,27 @@
+
+package br.com.bandtec.log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+class Log {
+    public Logger logger;
+    FileHandler criar;
+    
+    public Log(String nomeArq) throws SecurityException, IOException {
+        File arquivo = new File(nomeArq);
+        if(!arquivo.exists()){
+            arquivo.createNewFile();
+        }
+        criar = new FileHandler(nomeArq, true);
+        logger = logger.getLogger("test");
+        logger.addHandler(criar);
+        SimpleFormatter formatter = new SimpleFormatter();
+        criar.setFormatter(formatter);
+  
+    }
+    
+}
