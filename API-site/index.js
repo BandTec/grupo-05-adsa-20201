92e4f31a6ca486bf04aedec53fbb92fb26b59954
cervalhub <gabriel.meira@bandtec.com.br>
2020-11-20 20:18:20 -0300

    Subindo pasta API-Site atualizada com o login funcionando

--- API-site/index.js
+++ API-site/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 // const connection = require('./connection');
 const bodyParser = require('body-parser');
-const cors = require ('cors');
+const cors = require('cors');
 const port = 3000;
 const mssql = require('mssql');
 const request = new mssql.Request();
@@ -12,37 +12,46 @@ const request = new mssql.Request();
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //LOGIN
-app.post('/user', function (req,res){
+app.post('/user', function (req, res) {
     console.log("index.js está ok")
     console.log(req.body)
-    
-    let hh = request.query(`SELECT nomeFuncionario, senha FROM CadastroFuncionario where nomeFuncionario='${req.body.usuario1}' and senha='${req.body.senha1}' `,function(err,result){
-        if(err) throw err;        
-        res.send(result);
-        console.log(hh);
-    })
+
+    request.query(`SELECT * FROM 
+                    CadastroFuncionario where usuario='${req.body.usuario}' 
+                    and senha='${req.body.senha}' and cargo= '${req.body.cargo}'`,
+        function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            if (result.recordsets[0].length) {
+                res.json({
+                    msg: result.recordsets[0]
+                });
+            } else {
+                res.send(401)// não autorizado, QUANDO NAO ENCONTRAR USUARIO NO SELECT ACIMA 
+            }
+        })
 });
 
 
 
 //CADASTRO
-app.post('/addUser',(req,res)=>{
-    let data=[req.nomeFuncionario, req.body.email, req.body.senha, req.body.cargo];
+app.post('/addUser', (req, res) => {
+    let data = [req.nomeFuncionario, req.body.email, req.body.senha, req.body.cargo];
 
     let sql = "INSERT INTO dbo.CadastroFuncionario (nomeFuncionario, email, senha, cargo) values (?,?,?,?)";
 
-    connection.query(sql,data,function(err,result){
-        if(err) throw err;
+    connection.query(sql, data, function (err, result) {
+        if (err) throw err;
         res.send(result);
     });
 })
 
 
 //PORTA
-app.listen(port, function(){
-    console.log('Servidor rodando na porta '+ port);
+app.listen(port, function () {
+    console.log('Servidor rodando na porta ' + port);
 });
--- API-site/public/js/returnDados.js
+++ API-site/public/js/returnDados.js
@@ -3,7 +3,6 @@ let form = document.querySelector("#form_cadastro");
 let nome = document.querySelector('#nome_cadastro');
 let email = document.querySelector('#email_cadastro');
 let senha = document.querySelector('#senha_cadastro');
-let cargo = document.querySelector('#Select_cargo');
 let foto = document.querySelector('#Input_fotoPerfil');
 
 // variáveis que regataram o corpo dos inputs na tela login
@@ -16,27 +15,27 @@ let func = document.querySelector('#funcionarios');
 //----------------------------------------------------------------------//
 
 //VALIDAÇÃO CADASTRO
-function validaCadastro(evento){
-  evento.preventDefault();
-  let nome = document.querySelector('#user_login').value;
-  let senha1 = document.querySelector('#senha_login').value;
-  if(usuario1.indexOf('@') == -1 || usuario1.indexOf('.') == -1 || usuario1.length < 10 || senha1.length <= 5) {
-      alert('Email ou senha inválidos!')
-  }else{
-      addUser();
-  }
+function validaCadastro(evento) {
+    evento.preventDefault();
+    let nome = document.querySelector('#user_login').value;
+    let senha1 = document.querySelector('#senha_login').value;
+    if (usuario1.indexOf('@') == -1 || usuario1.indexOf('.') == -1 || usuario1.length < 10 || senha1.length <= 5) {
+        alert('Email ou senha inválidos!')
+    } else {
+        addUser();
+    }
 }
 
 //----------------------------------------------------------------------//
 
-function UserLogin(evento){
-    let ajax =  new XMLHttpRequest();
+function UserLogin(evento) {
+    let ajax = new XMLHttpRequest();
     let params = "user_login=" + usuario1.value + "&senha_login=" + senha1.value /*+ "&funcionarios" + func.value*/;
     console.log('chegou');
-    ajax.open("POST","http://localhost:3000/user");
-    ajax.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    ajax.onreadystatechange=function(){
-        
+    ajax.open("POST", "http://localhost:3000/user");
+    ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    ajax.onreadystatechange = function () {
+
     }
     ajax.send(params);
 }
@@ -44,43 +43,60 @@ function UserLogin(evento){
 //----------------------------------------------------------------------//
 
 // VALIDAÇÃO LOGIN
-function validaLogin(){
+function validaLogin() {
     event.preventDefault();
     let usuario1 = document.querySelector('#user_login').value;
     let senha1 = document.querySelector('#senha_login').value;
-    if(usuario1 == "" || senha1 == ""){
+    if (usuario1 == "" || senha1 == "") {
         alert('Verifique se todos os campos foram preenchidos!')
     }
-    else{
+    else {
         pegarusuario();
     }
-  }
+}
 
-  //----------------------------------------------------------------------//
+//----------------------------------------------------------------------//
 
-  let teste = [];
-  function pegarusuario(){
-    let usuario1 = document.querySelector('#user_login').value;
-    let senha1 = document.querySelector('#senha_login').value;
+let teste = [];
+function pegarusuario() {
+    console.log("CHEGOU");
+    let usuario = document.querySelector('#user_login').value;
+    let senha = document.querySelector('#senha_login').value;
+    let cargo = document.querySelector('#funcionarios').value;
     fetch("http://localhost:3000/user",
-    {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify({email:usuario1,senha:senha1})
-    }).then(res=> {
-        res.json().then(console.log)
-    })
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({ usuario, senha, cargo })
+        }).then(respostaDoServidor => {
+            if (respostaDoServidor.status === 200) {
+                respostaDoServidor.json().then(objetoDoBanco=> {
+                    if (objetoDoBanco.msg[0].cargo === 1) {
+                        window.location.assign('pagSuporte.html');
+                    }else if (objetoDoBanco.msg[0].cargo === 2){
+                        window.location.assign('pag-Dashboard/pag-dashboard.html');
+                    }
+                })
+            } else {
+                alert("usuario/senha invalido")
+            }
+            // if (res.status === 200) {
+            //     alert('Deu certo "loko" ')
+            // } else {
+            //     alert("Usuario/Senha 'invalido' ")
+            // }
+        })
 
-} 
-  
+}
+
 
 
 
 //----------------------------------------------------------------------//
-/* aqui estará adicionando eventos quando o input type submit for clicado 
+/* aqui estará adicionando eventos quando o input type submit for clicado
 no caso as funções disparadas serão addUser e UserLogin, cada uma com seu proprio evento. */
 
 ////////////////////////////////////////////////////||
